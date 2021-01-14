f03e141ae9cf934948e6f55a52b60a24e6b60121
Varun Kumar <heysadboy@gmail.com>
2021-01-14 21:45:01 +0900

    Adding grid system

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,5 @@ export default axios.create({
     baseURL: 'https://api.unsplash.com/',
     headers: {
         Authorization: 'Client-ID sJLBtJZLKNjaDVb5e94UGJ7q7LTtiN7gcGnO5zHaC00'
-    }
-
+    },
 });
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,12 +2,11 @@ import './ImageList.css';
 import React from 'react';
 import ImageCard from './ImageCard';
 
-const ImageList = (props) => {
-    const images = props.images.map(( image ) => {
+const ImageList = props => {
+    const images = props.images.map( image  => {
         return (<ImageCard key={image.id} image={image} />);
-    }
+    });
 
-    );
     return <div className="image-list">{images}</div>
 }
 
